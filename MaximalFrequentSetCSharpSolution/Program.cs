8dd3bc84b0287422fb95aab54e8a6d54f5bebb81
Benji Altman <benji@altmansoftwaredesign.com>
2017-03-01 13:56:15 -0600

    random generator is working

--- MaximalFrequentSetCSharpSolution/Program.cs
+++ MaximalFrequentSetCSharpSolution/Program.cs
@@ -12,23 +12,15 @@ namespace MaximalFrequentSet
 		public static void Main(string[] argv)
 		{
 			bool[][] input = new bool[1000][];
-			for(int i = 0; i < input.Length; i++)
+			for (int i = 0; i < input.Length; i++)
 			{
-				input[i] = new bool[16];
-				byte[] buff = new byte[2];
-				rand.NextBytes(buff);
-				using (System.IO.BinaryReader br = new System.IO.BinaryReader(new System.IO.MemoryStream(buff)))
-				{
-					ushort shor = br.ReadUInt16();
-					for (int j = 0; j < input[i].Length; j++)
-					{
-						input[i][j] = (shor & 1) == 1;
-					}
-					shor >>= 1;
-				}
+				bool[] row = new bool[16];
+				for (int j = 0; j < row.Length; j++)
+					row[j] = rand.Next(2) == 1;
+				input[i] = row;
 			}
 
-			TransactionSet my = new TransactionSet(input, 100);
+			TransactionSet my = new TransactionSet(input, 1);
 
 			Console.WriteLine(my.IsFrequent(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15));
 
@@ -39,13 +31,18 @@ namespace MaximalFrequentSet
 			 * 13
 			 */
 
-			var output = my.MaxFrequentSet();
-
-			foreach (var o in output)
+			for (int i = 0; i < input.Length; i++)
 			{
-				foreach (var p in o)
-					Console.Write(p);
-				Console.WriteLine();
+				var output = my.MaxFrequentSet();
+
+				//foreach (var o in output)
+				//{
+				//	foreach (var p in o)
+				//		Console.Write("{0}, ", p);
+				//	Console.WriteLine();
+				//}
+
+				Console.WriteLine(output.Count);
 			}
 		}
 	}
