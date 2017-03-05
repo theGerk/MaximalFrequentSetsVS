cf6fb717f753fde51b5835aca4ba338436798387
Benji Altman <benji@altmansoftwaredesign.com>
2017-03-04 21:45:19 -0600

    more work

--- MaximalFrequentSetCSharpSolution/Program.cs
+++ MaximalFrequentSetCSharpSolution/Program.cs
@@ -13,14 +13,21 @@ namespace MaximalFrequentSet
 
 		public static void Main(string[] argv)
 		{
-			TransactionSet ts = new TransactionSet("input.txt", 2);
-			foreach (var val in ts.MaxFrequentSet())
+			var ts = new TransactionSet("input.txt", 2).MaxFrequentSet();
+			using (var fs = File.CreateText("output.txt"))
 			{
-				var addIn = val.Reverse().ToArray();
-				Console.Write("{");
-				for (int i = 0; i < addIn.Length; i++)
-					Console.Write("{0}{1}", addIn[i], (i == addIn.Length - 1) ? "" : ", ");
-				Console.WriteLine("}");
+				foreach (var val in ts)
+				{
+					var addIn = val.Reverse().ToArray();
+					Console.Write("{");
+					for (int i = 0; i < addIn.Length - 1; i++)
+					{
+						fs.Write("{0}\t", addIn[i]);
+						Console.Write("{0},", addIn[i]);
+					}
+					fs.WriteLine(addIn[addIn.Length - 1]);
+					Console.WriteLine("{0}{1}", addIn[addIn.Length - 1], '}');
+				}
 			}
 		}
 	}
